aec20fd0eace86ff5943c67918a96c241732b7b3
shaktemanhatim <shaktemanhatim2@gmail.com>
2022-01-06 22:31:50 +0500

    Intial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+const nav = document.querySelector("nav");
+nav.addEventListener("mouseover", function run(e) {
+  if (e.target.classList.contains("lll")) {
+    let link = e.target;
+    let sibling = link.closest("nav").querySelectorAll("lll");
+    const logo = link.closest("nav").querySelector("p");
+    console.log("jover");
+    sibling.forEach(function (el) {
+      if (el !== sibling) {
+        el.style.opacity = 0.5;
+      }
+      logo.style.opacity = 0.5;
+    });
+  }
+});
